00b4cc867fa4f83c65f3807924155a4eb7a0b7c1
Matysek1 <82448809+Matysek1@users.noreply.github.com>
2025-06-16 22:12:13 +0200

    zmena

--- sablona1/src/pages/api/uploadthing.ts
+++
@@ -1,9 +0,0 @@
-// pages/api/uploadthing.ts
-import { createRouteHandler } from "uploadthing/server";
-import { ourFileRouter } from "../../server/uploadthing";
-export const runtime = "edge"; // <=== přidej toto
-
-
-export default createRouteHandler({
-  router: ourFileRouter,
-});
